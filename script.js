8ec78a9f78acefce345646502060f44cbfebca79
Neeraj Kumar <56425717+neerajdaksh@users.noreply.github.com>
2023-09-30 14:24:48 +0530

    feat: hamburger

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // ***select element***
 const body =document.querySelector("body");
 const modeBtn = document.querySelector(".circle");
+const showHamBtn = document.querySelector(".hambuger-btn");
+const closeBtn = document.querySelector("#close-btn");
+const hamburger = document.querySelector(".hamburger");
 
 
 
@@ -35,3 +38,27 @@ modeBtn.addEventListener("click",(e)=>{
     changeBtnMode(e.target);
    
 });
+
+
+// *********** hamburger show function ***********
+
+const showHamburger=()=>{
+
+hamburger.style.transform = 'translateY(0)';
+}
+
+// ### hide hamburger ###
+const hideHambuger=()=>{
+   hamburger.style.transform = "translateY(-11rem)";
+}
+
+
+showHamBtn.addEventListener("click",()=>{
+showHamburger();
+});
+
+closeBtn.addEventListener("click",()=>{
+    hideHambuger();
+});
+
+ 
